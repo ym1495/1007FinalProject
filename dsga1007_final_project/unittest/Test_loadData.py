999d098c56e88c12dd70ce53c1a828f3e0d62da0
yrmuriel820 <yrmuriel820@gmail.com>
2016-12-03 15:46:47 -0500

    project structure

---
+++ dsga1007_final_project/unittest/Test_loadData.py
@@ -0,0 +1,18 @@
+'''
+Created on Dec 3, 2016
+
+@author: muriel820
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
